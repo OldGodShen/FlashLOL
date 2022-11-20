6b6709120b946fd2238e00de21dd5c93b3109d44
OldGodShen <OldGodShen@SHENE14>
2022-11-20 20:10:04 +0800

    添加项目文件。

---
+++ FlashLOL/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace FlashLOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.User = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // User
+            // 
+            this.User.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.User.Location = new System.Drawing.Point(0, -2);
+            this.User.Multiline = true;
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(383, 90);
+            this.User.TabIndex = 0;
+            this.User.Text = "null";
+            this.User.TextChanged += new System.EventHandler(this.User_TextChanged);
+            // 
+            // B
+            // 
+            this.B.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.B.Location = new System.Drawing.Point(123, 96);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(150, 40);
+            this.B.TabIndex = 1;
+            this.B.UseVisualStyleBackColor = true;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 191);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.User);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "FlashLOL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox User;
+        private System.Windows.Forms.Button B;
+    }
+}
+
---
+++ FlashLOL/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace FlashLOL
+{
+    public partial class Form1 : Form
+	{
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+			Process process = new Process();
+			process.StartInfo.FileName = "netsh";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments = "advfirewall firewall add rule name=FlashLOL dir=out action=block program=\"" + this.User.Text + "\" enable=yes profile=public,private,domain";
+            process.Start();
+            process.WaitForExit();
+            System.Threading.Thread.Sleep(4000);
+            process.StartInfo.Arguments = "advfirewall firewall del rule name=FlashLOL dir=out";
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private void User_TextChanged(object sender, EventArgs e)
+        {
+			if(this.User.Text == "")
+            {
+				this.B.Enabled = false;
+				this.B.Text = "请输入游戏位置";
+            }
+            else
+            {
+				this.B.Enabled = true;
+				this.B.Text = "开刷！";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			this.User.Text = "";
+        }
+    }
+}
